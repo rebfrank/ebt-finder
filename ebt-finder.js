3686e6de692048e112bb733f85a920a0672d9edf
Rebecca Frank <rebeccaffrank@gmail.com>
2018-09-18 23:05:59 -0500

    Initial http get request of stores when map is moved
    - Still need to look at whether moveend is the desired event
    - Still need error handling for request
    - Right now just prints to console!

--- ebt-finder.js
+++ ebt-finder.js
@@ -1,3 +1,37 @@
+const ebtProvidersBaseUrl = "https://www.easyfoodstamps.com/"
+
+function onProvidersReceived(response)
+{
+    if (response.status !== 200) {
+        console.log('Problem with fetch. Status: ' + response.status);
+        return;
+    }
+    response.json().then(function(data) {
+        console.log(data);
+    });
+}
+
+function fetchProviders(latitude, longitude)
+{
+    const url = ebtProvidersBaseUrl + "stores?latitude=" + latitude +
+        "&longitude=" + longitude
+    fetch(url)
+        .then(onProvidersReceived)
+        .catch(function(err) {
+            console.log('Fetch error :-S', err);
+        });
+}
+
+function onGotCurrentPosition(position) {
+    ebtResourcesMap.panTo(L.latLng(position.coords.latitude,
+                          position.coords.longitude))
+}
+
+function onLocationChange(e) {
+    latLng = e.target.getCenter();
+    fetchProviders(latLng.lat, latLng.lng);
+}
+
 var ebtResourcesMap = L.map('mapid').setView([51.505, -0.09], 13);
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -5,10 +39,5 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     id: 'mapbox.streets',
     accessToken: 'pk.eyJ1IjoicmViZnJhbmsiLCJhIjoiY2ptOGIzcGZtMGs5cjN3bGt5dTM1bW1lbyJ9.-x71lERJRTdscGneZMFtUg'
 }).addTo(ebtResourcesMap);
-
-var currentPosition = navigator.geolocation.getCurrentPosition(
-    function (position) {
-        ebtResourcesMap.panTo(L.latLng(position.coords.latitude,
-                              position.coords.longitude))
-    }
-)
+ebtResourcesMap.on('moveend', onLocationChange)
+navigator.geolocation.getCurrentPosition(onGotCurrentPosition)
